4d9c83106a7905a81b239c9cadba3d9e536f24f4
Irina <irashilowa1997@mail.ru>
2024-05-17 22:30:29 +0300

    add junit4 tests

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,52 +1,52 @@
 package ru.netology.service;
 
-import static org.testng.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 public class CashbackHackServiceTest {
 
-    @org.testng.annotations.Test
+    @org.junit.Test
     public void shouldAmountEqualThousandRubles() {
         CashbackHackService service = new CashbackHackService();
         int amount = 1000; //сумма совершаемой покупки
         int actual = service.remain(amount);
         int expected = 0;
-        assertEquals(actual, expected);
+        assertEquals(expected, actual);
     }
 
-    @org.testng.annotations.Test
+    @org.junit.Test
     public void shouldAmountAboveThousandRubles() {
         CashbackHackService service = new CashbackHackService();
         int amount = 1001; //сумма совершаемой покупки
         int actual = service.remain(amount);
         int expected = 0; //сумма, на которую необходимо докупить, чтобы получить кешбек
-        assertEquals(actual, expected);
+        assertEquals(expected, actual);
     }
 
-    @org.testng.annotations.Test
+    @org.junit.Test
     public void shouldAmountBelowThousandRubles() {
         CashbackHackService service = new CashbackHackService();
         int amount = 999; //сумма совершаемой покупки
         int actual = service.remain(amount);
         int expected = 1; //сумма, на которую необходимо докупить, чтобы получить кешбек
-        assertEquals(actual, expected);
+        assertEquals(expected, actual);
     }
 
-    @org.testng.annotations.Test
+    @org.junit.Test
     public void shouldAmountEqualNineHundredRubles() {
         CashbackHackService service = new CashbackHackService();
         int amount = 900; //сумма совершаемой покупки
         int actual = service.remain(amount);
         int expected = 100; //сумма, на которую необходимо докупить, чтобы получить кешбек
-        assertEquals(actual, expected);
+        assertEquals(expected, actual);
     }
 
 
-    @org.testng.annotations.Test
+    @org.junit.Test
     public void shouldAmountEqualOneThousandOneHundredRubles() {
         CashbackHackService service = new CashbackHackService();
         int amount = 1100; //сумма совершаемой покупки
         int actual = service.remain(amount);
         int expected = 0; //сумма, на которую необходимо докупить, чтобы получить кешбек
-        assertEquals(actual, expected);
+        assertEquals(expected, actual);
     }
 }
