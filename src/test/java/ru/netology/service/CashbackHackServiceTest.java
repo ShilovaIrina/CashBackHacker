7e71aaa15b478f7badd3e02a90d5a93213eaa8b3
Irina <irashilowa1997@mail.ru>
2024-05-17 20:21:29 +0300

    add root testng

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,12 +1,52 @@
 package ru.netology.service;
 
-import org.testng.annotations.Test;
-
 import static org.testng.Assert.*;
 
 public class CashbackHackServiceTest {
 
-    @Test
-    public void testRemain() {
+    @org.testng.annotations.Test
+    public void shouldAmountEqualThousandRubles() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1000; //сумма совершаемой покупки
+        int actual = service.remain(amount);
+        int expected = 0;
+        assertEquals(actual, expected);
+    }
+
+    @org.testng.annotations.Test
+    public void shouldAmountAboveThousandRubles() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1001; //сумма совершаемой покупки
+        int actual = service.remain(amount);
+        int expected = 0; //сумма, на которую необходимо докупить, чтобы получить кешбек
+        assertEquals(actual, expected);
+    }
+
+    @org.testng.annotations.Test
+    public void shouldAmountBelowThousandRubles() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 999; //сумма совершаемой покупки
+        int actual = service.remain(amount);
+        int expected = 1; //сумма, на которую необходимо докупить, чтобы получить кешбек
+        assertEquals(actual, expected);
+    }
+
+    @org.testng.annotations.Test
+    public void shouldAmountEqualNineHundredRubles() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 900; //сумма совершаемой покупки
+        int actual = service.remain(amount);
+        int expected = 100; //сумма, на которую необходимо докупить, чтобы получить кешбек
+        assertEquals(actual, expected);
+    }
+
+
+    @org.testng.annotations.Test
+    public void shouldAmountEqualOneThousandOneHundredRubles() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1100; //сумма совершаемой покупки
+        int actual = service.remain(amount);
+        int expected = 0; //сумма, на которую необходимо докупить, чтобы получить кешбек
+        assertEquals(actual, expected);
     }
 }
